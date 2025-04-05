12a9f89813bc312864d28af727615bb6ef12b2e0
Vinicius <vinicius2145.god@gmail.com>
2025-04-04 21:52:01 -0300

    exerciocios

--- script.js
+++ script.js
@@ -1,3 +1,119 @@
-alert("Hello, world na tela!")
+function exercios01() {
 
-console.log("hello, world no console!")
+    let idade = prompt("digite sua idade")
+
+    if (idade >= 18) {
+        alert("voce e maior de idade")
+    } else {
+        alert("voce e menor de idade")
+
+    }
+
+}
+
+
+
+function exercios02() {
+
+
+    let numero = prompt("digite um numero")
+
+    if (numero >= 0) {
+        alert("o numero e positivo")
+    } else {
+        alert("o numero e negativo")
+
+
+
+    }
+
+
+
+}
+
+
+function exercios03() {
+
+    let nota = prompt("digite sua nota")
+
+    if (nota >= 60) {
+        alert("voce foi aprovado")
+    } else {
+        alert("voce foi reprovado")
+
+
+    }
+
+
+}
+
+function exercios04() {
+
+
+
+    let numero = prompt("digite um numero")
+
+
+    if (numero > 0) {
+        
+        alert("o numero e postivo")
+
+        
+
+    }
+
+    else{
+        if (numero < 0) {
+            alert("o numero e negativo")
+        } else {
+            alert("o numero e neutro")
+        }
+
+    }
+
+}
+
+
+function exercios05(){
+
+    let numero = prompt("digite um numero")
+
+
+    if (numero %  2 == 0){
+        alert("esse numero e divisivel")
+    }else{
+        alert("esse numero nao e divisivel")
+
+
+    }
+
+
+
+
+
+}
+
+function exercios06(){
+
+    
+
+    let idade = prompt("digite sua idade")
+
+    if(idade <=13){
+        alert("crianca") 
+    }
+
+    else{
+        if( idade <=17)
+         alert("adolescente")   
+    }
+
+    
+    
+        
+
+    
+    
+    
+}
+exercios06()
